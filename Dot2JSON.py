619eb4ea4c20fbe2cbd7b221c2c144666c9a5375
ajing <ajing@ajing-desktop.(none)>
2013-12-12 00:05:50 -0500

    working version to parse dot to a tree structure

--- Dot2JSON.py
+++ Dot2JSON.py
@@ -37,8 +37,7 @@ class Node(dict):
         self.parent = None
         self.children = []
         self.dist   = 0
-        if not attr is None:
-            self.update(attr)
+        self.update(attr)
 
     def __str__(self):
         return "a node with name:" + self.name
@@ -64,19 +63,61 @@ class Node(dict):
             raise TypeError("argument should be Node class")
         self.parent = a_node
 
+    def set_dist(self, dist):
+        self.dist = dist
+
+def NodeByName(name, contents):
+    for eachline in contents:
+        if NodeNameExist(eachline) and not IsEdge(eachline) and name in eachline:
+            name, width, group = GetNodeProperty(eachline)
+            size = GetSize(width)
+            return Node(name, size = size, group = group)
+
+def AddNewChild(contents, a_node, new_node_name, edge_length, childrens, currentlist):
+    # return a node object
+    newnode = NodeByName(new_node_name, contents)
+    newnode.set_dist(edge_length)
+    childrens.append(newnode)
+    currentlist.append(new_node_name)
+
+def ExtendChildren(a_node, contents, cur_list):
+    children_list = []
+    for eachline in contents:
+        if IsEdge(eachline):
+            name, attr = NameAndAttribute(eachline)
+            fnode, snode = ProcessName(name, True)
+            if fnode == a_node.name and not snode in cur_list:
+                edge_len  = GetAttributeValue("len", eachline)
+                AddNewChild(contents, a_node, snode, edge_len, children_list, cur_list)
+            if snode == a_node.name and not fnode in cur_list:
+                edge_len  = GetAttributeValue("len", eachline)
+                AddNewChild(contents, a_node, fnode, edge_len, children_list, cur_list)
+    return children_list
+
+def Root2JSON(root):
+    cur_node = root
+    for eachchild in
+
 def Dot2JSON(dotfile):
     # dotfile is a dot file
     contents = open(dotfile).readlines()
     # get the root of the network
     root = GetRoot(dotfile)
-    for eachline in contents:
-        if NodeNameExist(eachline):
-            if not IsEdge(eachline):
-                pass
+    curr_nodes = [root]
+    curr_name_list = [root.name]
+    next_nodes     = 1
+    while next_nodes:
+        next_nodes = []
+        for each_node in curr_nodes:
+            next_nodes += ExtendChildren(each_node, contents, curr_name_list)
+        curr_nodes = next_nodes
 
 def test():
     testfile = "test.gv"
-    print GetRoot(testfile)
+    root = GetRoot(testfile)
+    print root.__dict__
+    print root["size"]
+    Dot2JSON(testfile)
 
 if __name__ == "__main__":
     test()
