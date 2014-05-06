2493c0ce1c6cc5100463c1835126cf087d4e752b
ajing <ajingnk@gmail.com>
2014-05-06 00:09:45 -0400

    save as svg is working

--- Large/savepng.js
+++ Large/savepng.js
@@ -4,17 +4,22 @@ d3.select("#savepng").on("click", function(){
         .attr("version", 1.1)
         .attr("xmlns", "http://www.w3.org/2000/svg")
         .node().parentNode.innerHTML;
-  console.log(html);
+  //console.log(html);
   var canvas = document.getElementById("canvas");
   canvg(canvas, html);
   var a = document.createElement("a");
-  a.download = "sample.svg";
-  a.href = canvas.toDataURL("image/svg");
+  a.download = "sample.png";
+  a.href = canvas.toDataURL("image/png");
   a.click();
 });
 
 d3.select("#savesvg").on("click", function(){
-  d3.select(this)
-    .attr("href", 'data:application/octet-stream;base64,' + btoa(d3.select(".intgraph svg").html()))
-    .attr("download", "sample.svg")
+  var html = d3.select(".intgraph svg")
+        .attr("version", 1.1)
+        .attr("xmlns", "http://www.w3.org/2000/svg")
+        .node().parentNode.innerHTML;
+  var a = document.createElement("a");
+  a.download = "sample.svg";
+  a.href = 'data:application/octet-stream;base64,' + btoa(html);
+  a.click();
 })
