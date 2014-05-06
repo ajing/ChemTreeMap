4d71ac0257ca136a5065c6b1c97a95314d7098cf
ajing <ajingnk@gmail.com>
2014-05-05 23:45:54 -0400

    save function for svg

--- Large/savepng.js
+++ Large/savepng.js
@@ -4,35 +4,11 @@ d3.select("#save").on("click", function(){
         .attr("version", 1.1)
         .attr("xmlns", "http://www.w3.org/2000/svg")
         .node().parentNode.innerHTML;
-  //console.log(html);
-  var imgsrc = 'data:image/svg+xml;base64,'+ btoa(html);
-  var canvas = document.querySelector("canvas"),
-  context = canvas.getContext("2d");
-  var image = new Image;
-  image.src = imgsrc;
-  image.onload = function() {
-  context.drawImage(image, 0, 0);
-  //save and serve it as an actual filename
-  binaryblob();
+  console.log(html);
+  var canvas = document.getElementById("canvas");
+  canvg(canvas, html);
   var a = document.createElement("a");
   a.download = "sample.png";
   a.href = canvas.toDataURL("image/png");
   a.click();
-  };
 });
-
-function binaryblob(){
-    var byteString = atob(document.querySelector("canvas").toDataURL().replace(/^data:image\/(png|jpg);base64,/, "")); //wtf is atob?? https://developer.mozilla.org/en-US/docs/Web/API/Window.atob
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    var dataView = new DataView(ab);
-    var blob = new Blob([dataView], {type: "image/png"});
-    var DOMURL = self.URL || self.webkitURL || self;
-    var newurl = DOMURL.createObjectURL(blob);
-    var img = '<img src="'+newurl+'">';
-    d3.select("#img").html(img);
-}
-
