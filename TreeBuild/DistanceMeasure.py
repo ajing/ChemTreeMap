3512f6e45a1f716466182d667dbf3ca7c74bdf4f
ajing <ajingnk@gmail.com>
2015-08-24 09:54:29 -0400

    similarity file

--- TreeBuild/DistanceMeasure.py
+++ TreeBuild/DistanceMeasure.py
@@ -1,7 +1,24 @@
 '''
     Measure and write the distance matrix for the whole molecule library
 '''
+import numpy as np
+
+SMILE_COLUMNNAME = "Smiles"
+
+def DistanceMatrix(smile_list):
+    d_matrix = np.zeros()
+    list_len = len(smile_list)
+    for i in range(list_len):
+        for j in range
+
+def WriteAsPHYLIPFormat(d_matrix, ligand_name):
+    list_len = len(ligand_name)
+    for i in range(list_len)
+    
+    return filename
 
 def GenerateDistanceFile(ligand_dict):
-    for ligname in ligand_dict.keys():
+    smile_list = [ligand_dict[lig_name][SMILE_COLUMNNAME] for lig_name in ligand_dict.keys()]
+    d_matrix   = DistanceMatrix(smile_list)
+    filename   = WriteAsPHYLIPFormat(d_matrix, ligand_dict.keys())
     return filename
