a2e9ddba9cab5b79a43192389bf6ed467f810d55
ajing <ajingnk@gmail.com>
2015-08-26 22:06:14 -0400

    cluster molecules

--- TreeBuild/ClusterMolecules.py
+++ TreeBuild/ClusterMolecules.py
@@ -2,7 +2,7 @@
     Clustering Large Scale Molecule Library
 '''
 
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN, MiniBatchKMeans
 from numpy import arange
 import numpy as np
 from DistanceMeasure import getSimilarity
@@ -23,9 +23,8 @@ def GetCenterOfCluster(fp_list, idx):
     return clusters[len(clusters) / 2]
 
 def GetSmallSetAfterClustering(fp_list):
-    dbs = DBSCAN(eps = 0.8, min_samples = 2, metric = partial(GetDistance, fp_list))
+    dbs = DBSCAN(eps = 0.8, min_samples = 2, metric = partial(GetDistance, fp_list), algorithm='ball_tree')
     dbs.fit(arange(len(fp_list)).reshape(-1, 1))
-    core_idx = dbs.core_sample_indices_
     gp_name  = set(dbs.labels_)
     labels   = dbs.labels_
     center_list= []
@@ -35,8 +34,19 @@ def GetSmallSetAfterClustering(fp_list):
         class_member_idx = np.where(labels == each_gp)[0]
         center = GetCenterOfCluster(fp_list, class_member_idx)
         center_list.append(center)
+    print labels
     print len(center_list)
     return center_list
+
+def GetSmallSetAfterClusteringKMeans(fp_list):
+    #mnb = MiniBatchKMeans(n_clusters = 10000, metric = partial(GetDistance, fp_list))
+    #mnb = MiniBatchKMeans(n_clusters = 2, metric = partial(GetDistance, fp_list))
+    #mnb.fit(arange(len(fp_list)).reshape(-1, 1))
+    #center_idx = mnb.cluster_centers_
+    #mnb.fit(arange(len(fp_list)).reshape(-1, 1))
+    #center_idx = mnb.cluster_centers_
+    pass
+
 
 def WriteToFile():
     pass
@@ -46,7 +56,9 @@ if __name__ == "__main__":
     from Util import ParseLigandFile
     from Model import SMILE_COLUMNNAME, FILE_FORMAT
     from DistanceMeasure import ToFPObj
-    ligand_dict = ParseLigandFile("./Data/chembl_20_new.txt")
+    #ligand_dict = ParseLigandFile("./Data/chembl_20_new.txt")
+    #ligand_dict = ParseLigandFile("./Data/result_clean_no0.txt")
+    ligand_dict = ParseLigandFile("./Data/chembl_20_10000.txt")
     smile_list = [ [lig_name, ligand_dict[lig_name][SMILE_COLUMNNAME]] for lig_name in ligand_dict.keys()]
     fp_list    = ToFPObj(smile_list)
     GetSmallSetAfterClustering(fp_list)
