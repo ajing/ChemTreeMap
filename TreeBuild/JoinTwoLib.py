fe407ceb7f7161d02aa7a6811a81e353c1017335
ajing <ajingnk@gmail.com>
2015-10-02 10:14:10 -0400

    updates join tow libs

--- TreeBuild/JoinTwoLib.py
+++ TreeBuild/JoinTwoLib.py
@@ -1,9 +1,10 @@
 '''
     Joining two molecule libraries
 '''
+import pickle
 
 def JoinTwoLib(infile1, infile2):
-    fp1list = 
-    fp2list = 
+    fp1list = pickle.load
+    fp2list = pickle.load
     infile3 = JoinTwoFPList()
     return infile3
