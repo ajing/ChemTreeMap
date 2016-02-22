f77f6886a300b7aaba40141dc4a79760f3acc490
ajing <ajingnk@gmail.com>
2016-02-22 11:32:29 -0500

    new setup

--- backend/setup.py
+++ backend/setup.py
@@ -17,10 +17,11 @@ setup(
     license='Apache 2.0',
     author='ajing',
     author_email='ajingnk@gmail.com',
-    description='Generate Tree Structures for Biochemical Similarity in Molecular Datasets.',
+    description='Generate Tree Structures for Biochemical Similarity in Molecular Datasets. Please install rdkit and graphviz first, because they are not pip installable.',
     install_requires=[
         #'rdkit',  ## currently rdkit is not pip installable
         #               ## so this dependency must be met by the installer
         'ete2'
     ],
+    package_data={'treebuild': ['data/*.txt', 'data/*.py', 'lib/rapidnj-linux-64']}
 )
