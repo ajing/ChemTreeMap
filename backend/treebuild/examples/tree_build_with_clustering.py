92cff3111b28998c8d6edb6498362b3e9c82f46b
Jing Lu <jing.lu@veracyte.com>
2016-11-28 14:28:48 -0800

    example for allosteric and competitive clustering

--- backend/treebuild/examples/tree_build_with_clustering.py
+++ backend/treebuild/examples/tree_build_with_clustering.py
@@ -12,10 +12,32 @@
 # ligandid[tab]smile[tab]allosteric
 
 from treebuild import TreeBuild
+from treebuild import DEFAULT_FINGERPRINT_TYPES
+from sklearn.cluster import KMeans
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+
+def SMILE2Matrix(smile_list):
+    # To ECFP6
+    def ToECFP(smile):
+        mol = Chem.MolFromSmiles(smile)
+        return AllChem.GetMorganFingerprint(mol, 3)
+
+    fps = map(ToECFP, smile_list)
+
+    np_fps = []
+    for fp in fps:
+      arr = numpy.zeros((1,))
+      DataStructs.ConvertToNumpyArray(fp, arr)
+      np_fps.append(arr)
+
+    print np_fps
 
 
 
-def LigandClusteringByClass(lig_dict, class_col = "allosteric"):
+def LigandClusteringByClass(lig_dict, class_col = "allosteric", num_clusters = {"allo": 5, "comp" : 3}):
     """
     Ligand clustering by ligand class
 
@@ -23,16 +45,29 @@ def LigandClusteringByClass(lig_dict, class_col = "allosteric"):
     :param class_col: the column name with ligand class
     :return: new lig_dict with cluster size
     """
+    smile_list = [ [lig_name, liganddict[lig_name][SMILE_COLUMNNAME]] for lig_name in liganddict.keys()]
 
+    # to fp matrix
+    fp_mat = SMILE2Matrix(smile_list)
+    print fp_mat.shape
+    print fp_mat
+    return fp_mat
 
-def MakeAlloFile(filename):
-    """
-        
-    """
+    # KMean clustering
+    # allo_c <- KMeans(n_clusters = num_clusters["allo"]).fit()
+    #
+    # print "finish parsing smile list"
+    # list_len = len(fp_list)
+    #
+    # newfilename = datetime.datetime.now().strftime(FILE_FORMAT) + ".dist"
+    # fileobj  = open(newfilename, "w")
+    # fileobj.write( str(list_len) + "\n")
 
 if __name__ == "__main__":
     input_file = "allo.txt"
     lig_dict = TreeBuild.parse_lig_file(input_file, "ligandid")
+
+    LigandClusteringByClass(lig_dict)
 
     # distfile = TreeBuild.gen_dist_file(lig_dict, fp.fp_func)
     # newick_o = TreeBuild.run_rapidnj(distfile)
